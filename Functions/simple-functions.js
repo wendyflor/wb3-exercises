ac441ca911c8ec4179f418280d7718b80ef65013
Wendy Flores <wendyf@appdev.yearup.org>
2023-05-01 10:34:34 -0400

    Functions commit

---
+++ Functions/simple-functions.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function favoriteThings() {
+    console.log('Name: Wendy Flores');
+    console.log('Favorite Movie: Monsters Inc');
+    console.log('Favorite Band: Ateez');
+}
+favoriteThings();
+
+function whyImHere() {
+    console.log('I joined this Learning to Code program in order to acquire new skills.');
+}
+whyImHere();
+
+function favoritePlaces() {
+    console.log('One of my favorite places to visit is the beach but at night once the sun starts setting. I like it because it is peaceful and calming.');
+}
+
+favoritePlaces();
